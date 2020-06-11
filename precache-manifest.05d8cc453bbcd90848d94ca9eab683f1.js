ce907245854d1ecb91db09773bc31f9228076317
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2020-06-10 23:23:18 -0500

    Updates

---
+++ precache-manifest.05d8cc453bbcd90848d94ca9eab683f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790b51d769e7a696af674fc6697e6b28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557bd2838ba476455a18",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a59dc853b6460313637c",
+    "url": "/static/css/main.72554422.chunk.css"
+  },
+  {
+    "revision": "557bd2838ba476455a18",
+    "url": "/static/js/2.e95201b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e95201b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59dc853b6460313637c",
+    "url": "/static/js/main.8671dbf6.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  }
+]);
