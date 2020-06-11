718ac89c7923f68b02dc27f2eae2ab93dbf1696e
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2020-06-10 22:57:05 -0500

    Add Scroll component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { useState, useEffect } from "react";
 import debounce from "lodash/debounce";
+import axios from "axios";
 import Card from "components/Card";
 import SearchBox from "components/SearchBox";
-import ROBOTS from "data/robots";
+import Scroll from "components/Scroll";
 
 function App() {
-  const [robots, setRobots] = useState(ROBOTS);
+  const [robots, setRobots] = useState([]);
+  const [filteredRobots, setFilteredRobots] = useState([]);
   const [search, setSearch] = useState("");
+
+  const fetchRobots = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setRobots(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRobots();
+  }, []);
 
   const onSearchChange = debounce((value) => {
     setSearch(value);
   }, 300);
 
   useEffect(() => {
-    const filteredRobots = ROBOTS.filter((robot) =>
+    const filteredRobots = robots.filter((robot) =>
       robot.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
     );
 
-    search.length ? setRobots(filteredRobots) : setRobots(ROBOTS);
-  }, [search]);
+    setFilteredRobots(filteredRobots);
+  }, [search, robots]);
 
   return (
     <div className="tc">
-      <h1 className="f-headline mb3 white">RoboFriends</h1>
+      <h1 className="f1 lh-title mt4 mb3 white">RoboFriends</h1>
       <SearchBox onChange={(e) => onSearchChange(e.target.value)} />
-      {robots.map((robot, index) => (
-        <Card key={index} {...robot} />
-      ))}
+      <Scroll>
+        {filteredRobots.map((robot, index) => (
+          <Card key={index} {...robot} />
+        ))}
+      </Scroll>
     </div>
   );
 }
---
+++ src/components/Scroll/Scroll.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Scroll.styles.css";
+
+const Scroll = ({ children }) => {
+  return <div className="scroll">{children}</div>;
+};
+export default Scroll;
